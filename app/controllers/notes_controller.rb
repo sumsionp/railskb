9dbfa2e5d6550473fa206a359b4ae93824316548
Peter Sumsion <sumsionp@gmail.com>
2014-05-15 07:23:47 -0600

    Add controller action, new, and form template for note
    Add :notice flash to layout

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,4 +6,24 @@ class NotesController < ApplicationController
   def show
     @note = Note.find(params[:id])
   end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+
+    if @note.save
+      redirect_to new_note_path, notice: "You're note is safely saved!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :text)
+  end
 end
